60b48b6d88b606c0e8b9ae8f002094cc64afbaef
missingzx@126.com <missingzx@126.com>
2019-10-19 15:43:27 +0800

    hibernate_sql学习

---
+++ src/com/hibernate01/CustomerTest.java
@@ -0,0 +1,35 @@
+package com.hibernate01;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void Test() {
+		
+		Configuration configuration=new Configuration().configure();
+		
+		SessionFactory buildSessionFactory = configuration.buildSessionFactory();
+		Session openSession = buildSessionFactory.openSession();
+		
+		Transaction beginTransaction = openSession.beginTransaction();
+		
+		try {
+			Customer customer = new Customer();
+//			customer.setCust_id(1);
+			customer.setCust_name("测试2");
+			openSession.save(customer);
+			beginTransaction.commit();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		openSession.close();
+		buildSessionFactory.close();
+		
+		
+	}
+}
